5cc1fdaa82b253fc523d6e994c565fc57ff3b331
Alexey G. Standalone <coder2k@mail.ru>
2021-06-15 21:08:01 +0300

    user models

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,55 @@
+class UserController {
+    async registration (req, res, next) {
+        try {
+
+        }
+        catch (e) {
+
+        }
+    }
+    async login (req, res, next) {
+        try {
+
+        }
+        catch (e) {
+
+        }
+    }
+    async logout (req, res, next) {
+        try {
+
+        }
+        catch (e) {
+
+        }
+    }
+
+    async activate (req, res, next) {
+        try {
+
+        }
+        catch (e) {
+
+        }
+    }
+
+    async refreshToken (req, res, next) {
+        try {
+
+        }
+        catch (e) {
+
+        }
+    }
+
+    async getUser (req, res, next) {
+        try {
+            res.json(['123','456'])
+        }
+        catch (e) {
+
+        }
+    }
+}
+
+module.exports = new UserController()
